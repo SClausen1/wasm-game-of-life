2bcc0d3b9ab4c945cb61fa0d05d2cd50c58ed885
sclausen1 <stevenclausen1@gmail.com>
2023-02-11 20:17:37 -0500

    adds ability to calculate multiple generations per tick

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,35 @@
 mod utils;
 
-use std::fmt;
-use wasm_bindgen::prelude::*;
-
 extern crate js_sys;
 extern crate web_sys;
+
+use std::fmt;
+use wasm_bindgen::prelude::*;
+use web_sys::console;
+
+
+
+pub struct Timer<'a> {
+    name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        console::time_with_label(name);
+        Timer { name }
+    }
+}
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        console::time_end_with_label(self.name);
+    }
+}
 
 // A macro to provide `println!(..)`-style syntax for `console.log` logging.
 macro_rules! log {
     ( $( $t:tt )* ) => {
-        web_sys::console::log_1(&format!( $( $t )* ).into());
+        console::log_1(&format!( $( $t )* ).into());
     }
 }
 
@@ -55,6 +75,7 @@ impl Cell {
 pub struct Universe {
     width: u32,
     height: u32,
+    generations_per_tick : u32,
     cells: Vec<Cell>,
 }
 
@@ -65,22 +86,44 @@ impl Universe {
         utils::set_panic_hook();
         let width = 64;
         let height = 64;
-
-        let cells = (0..width * height)
-            .map(|_| {
-                if js_sys::Math::random() > 0.5 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            })
-            .collect();
-
+        let generations_per_tick = 1;
+        let cells =(0..width * height)
+        .map(|_| {
+            if js_sys::Math::random() > 0.5 {
+                Cell::Alive
+            } else {
+                Cell::Dead
+            }
+        })
+        .collect();
         Universe {
             width,
             height,
+            generations_per_tick,
             cells,
         }
+    }
+    
+    pub fn set_generations_per_tick(& mut self, updated_ticks : u32){
+        self.generations_per_tick = updated_ticks;
+    }
+
+    fn generate_cells(&self, width: u32, height: u32) -> Vec<Cell>{
+        return (0..width * height)
+        .map(|_| {
+            if js_sys::Math::random() > 0.5 {
+                Cell::Alive
+            } else {
+                Cell::Dead
+            }
+        })
+        .collect();
+        
+    }
+   
+    pub fn reset(& mut self){
+        let next = self.generate_cells(self.width, self.height);
+        self.cells = next;
     }
 
     pub fn render(&self) -> String {
@@ -93,45 +136,85 @@ impl Universe {
 
     fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
         let mut count = 0;
-        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
-                if delta_row == 0 && delta_col == 0 {
-                    continue;
-                }
-
-                let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_col = (column + delta_col) % self.width;
-                let index = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[index] as u8;
-            }
-        }
-        return count;
+    
+        let north = if row == 0 {
+            self.height - 1
+        } else {
+            row - 1
+        };
+    
+        let south = if row == self.height - 1 {
+            0
+        } else {
+            row + 1
+        };
+    
+        let west = if column == 0 {
+            self.width - 1
+        } else {
+            column - 1
+        };
+    
+        let east = if column == self.width - 1 {
+            0
+        } else {
+            column + 1
+        };
+    
+        let nw = self.get_index(north, west);
+        count += self.cells[nw] as u8;
+    
+        let n = self.get_index(north, column);
+        count += self.cells[n] as u8;
+    
+        let ne = self.get_index(north, east);
+        count += self.cells[ne] as u8;
+    
+        let w = self.get_index(row, west);
+        count += self.cells[w] as u8;
+    
+        let e = self.get_index(row, east);
+        count += self.cells[e] as u8;
+    
+        let sw = self.get_index(south, west);
+        count += self.cells[sw] as u8;
+    
+        let s = self.get_index(south, column);
+        count += self.cells[s] as u8;
+    
+        let se = self.get_index(south, east);
+        count += self.cells[se] as u8;
+    
+        count
     }
-
+    
     pub fn tick(&mut self) {
+        // let _timer = Timer::new("Universe::tick");
         let mut next = self.cells.clone();
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let index = self.get_index(row, col);
-                let current_cell = self.cells[index];
-                let live_neighbors = self.live_neighbor_count(row, col);
-                let next_cell = match (current_cell, live_neighbors) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (Cell::Dead, 3) => Cell::Alive,
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
-                };
-                next[index] = next_cell;
+        for _generation in 0..self.generations_per_tick{
+            for row in 0..self.height {
+                for col in 0..self.width {
+                    let index = self.get_index(row, col);
+                    let current_cell = self.cells[index];
+                    let live_neighbors = self.live_neighbor_count(row, col);
+                    let next_cell = match (current_cell, live_neighbors) {
+                        // Rule 1: Any live cell with fewer than two live neighbours
+                        // dies, as if caused by underpopulation.
+                        (Cell::Alive, x) if x < 2 => Cell::Dead,
+                        // Rule 2: Any live cell with two or three live neighbours
+                        // lives on to the next generation.
+                        (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                        // Rule 3: Any live cell with more than three live
+                        // neighbours dies, as if by overpopulation.
+                        (Cell::Alive, x) if x > 3 => Cell::Dead,
+                        // Rule 4: Any dead cell with exactly three live neighbours
+                        // becomes a live cell, as if by reproduction.
+                        (Cell::Dead, 3) => Cell::Alive,
+                        // All other cells remain in the same state.
+                        (otherwise, _) => otherwise,
+                    };
+                    next[index] = next_cell;
+                }
             }
         }
         // log!("Next cells are {:?}", next);
@@ -162,7 +245,7 @@ impl Universe {
 
     pub fn toggle_cell(&mut self, row: u32, column: u32) {
         let idx = self.get_index(row, column);
-        log!("toggle cell called with row {:?}", idx);
+        // log!("toggle cell called with row {:?}", idx);
         self.cells[idx].toggle();
         
     }
--- www/index.js
+++ www/index.js
@@ -1,24 +1,30 @@
 import { Universe, Cell } from "wasm-game-of-life";
 import { memory } from "wasm-game-of-life/wasm_game_of_life_bg"
-const CELL_SIZE = 10; // px
+const CELL_SIZE = 7; // px
 const GRID_COLOR = "#ffbe0b";
 const DEAD_COLOR = "#fb5607";
 const ALIVE_COLOR = "#8338ec";
 
 const canvas = document.getElementById("game-of-life-canvas")
 const universe = Universe.new()
+universe.set_height(64)
+universe.set_width(64)
+universe.reset()
 const width = universe.width()
 const height = universe.height()
 const ctx = canvas.getContext("2d")
 let animationId = null
-// let ticksPerRender = 1
 
 canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
 
 const playPauseButton = document.getElementById("play-pause");
 
-// const tickSlider = document.getElementById("tick-slider")
+const resetUniverButton = document.getElementById("reset-universe");
+
+const tickSlider = document.getElementById("tick-slider")
+
+const tickSlideMessage = document.getElementById("tick-slider-text")
 
 const play = () => {
     playPauseButton.textContent = "⏸";
@@ -43,9 +49,16 @@ playPauseButton.addEventListener("click", event => {
         pause()
     }
 })
-// tickSlider.addEventListener("input" , event => {
-//     ticksPerRender = event.currentTarget.value ? 1 : 1
-// })
+
+resetUniverButton.addEventListener("click", event => {
+    universe.reset()
+})
+
+tickSlider.addEventListener("input", event => {
+    const ticksPerRender = event.currentTarget.value ? event.currentTarget.value : 1
+    universe.set_generations_per_tick(ticksPerRender)
+    tickSlideMessage.textContent = `Number of generations per tick: ${ticksPerRender}`
+})
 
 canvas.addEventListener("click", event => {
     const boundingRect = canvas.getBoundingClientRect()
@@ -54,8 +67,8 @@ canvas.addEventListener("click", event => {
     const scaleY = canvas.height / boundingRect.height
 
 
-    const clickX =( event.clientX - boundingRect.left) * scaleX
-    const clickY =( event.clientY - boundingRect.top) * scaleY
+    const clickX = (event.clientX - boundingRect.left) * scaleX
+    const clickY = (event.clientY - boundingRect.top) * scaleY
 
     const column = Math.min(Math.floor(clickX / (CELL_SIZE + 1)), width - 1)
     const row = Math.min(Math.floor(clickY / (CELL_SIZE + 1), height - 1));
@@ -96,14 +109,14 @@ const drawCells = () => {
 
     ctx.beginPath();
 
+    ctx.fillStyle = ALIVE_COLOR
+
     for (let row = 0; row < height; row++) {
         for (let col = 0; col < width; col++) {
             const idx = getIndex(row, col);
-
-            ctx.fillStyle = cells[idx] === Cell.Dead
-                ? DEAD_COLOR
-                : ALIVE_COLOR;
-
+            if (cells[idx] !== Cell.Alive) {
+                continue
+            }
             ctx.fillRect(
                 col * (CELL_SIZE + 1) + 1,
                 row * (CELL_SIZE + 1) + 1,
@@ -113,14 +126,69 @@ const drawCells = () => {
         }
     }
 
+
+    ctx.fillStyle = DEAD_COLOR
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+            if (cells[idx] !== Cell.Dead) {
+                continue
+            }
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
     ctx.stroke();
 }
 
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps")
+        this.frames = []
+        this.lastFrameTimeStamp = performance.now()
+    }
+
+    render() {
+        const now = performance.now()
+        const delta = now - this.lastFrameTimeStamp
+        this.lastFrameTimeStamp = now
+        const fps = 1 / delta * 1000
+        this.frames.push(fps)
+
+        if (this.frames.length > 100) {
+            this.frames.shift()
+        }
+
+        const frame_length = this.frames.length
+        let min = Infinity
+        let max = - Infinity
+        let sum = 0
+        for (let i = 0; i < frame_length; i++) {
+            sum += this.frames[i]
+            min = Math.min(min, this.frames[i])
+            max = Math.max(max, this.frames[i])
+        }
+
+        let mean = sum / frame_length
+
+        // Render the statistics.
+        this.fps.textContent = `
+        Frames per Second:
+                latest = ${Math.round(fps)}
+        avg of last 100 = ${Math.round(mean)}
+        min of last 100 = ${Math.round(min)}
+        max of last 100 = ${Math.round(max)}
+        `.trim();
+    }
+}
 
 const renderLoop = () => {
-    // for(let i = 0; i < ticksPerRender; i++){
-        
-    // }
+    fps.render()
+
     universe.tick()
 
     drawGrid()
